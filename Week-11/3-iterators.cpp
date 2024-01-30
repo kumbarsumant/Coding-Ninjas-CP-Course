7ccfbd97efaea18943fe06f716f0f30bab2e2268
Sumant Kumbar <my.personal.sumanth@gmail.com>
2024-01-30 21:30:28 +0530

    update iterator.cpp

--- Week-11/3-iterators.cpp
+++ Week-11/3-iterators.cpp
@@ -19,16 +19,33 @@ int main() {
     it++;
   }
 
+  unordered_map<string, int>::iterator it1 = ourmap.find("abc1");
+  cout << "The value for key abc1 is " << it1->second << endl;
+
+  // throws error (segmentation fault) if the key is not present.
+  // unordered_map<string, int>::iterator it3 = ourmap.find("abc");
+  // cout << "The value for key abc1 is " << it3->second;
+
+  // Deleting the key using the iterator
+  ourmap.erase(it1);
+
+  unordered_map<string, int>::iterator it3 = ourmap.begin();
+  // order is not guaranteed
+  while (it3 != ourmap.end()) {
+    cout << "Key: " << it3->first << " " << "Value: " << it3->second << endl;
+    it3++;
+  }
+
   vector<int> v;
   v.push_back(1);
   v.push_back(2);
   v.push_back(3);
   v.push_back(4);
 
-  vector<int>::iterator it1 = v.begin();
-  while (it1 != v.end()) {
-    cout << *it1 << endl;
-    it1++;
+  vector<int>::iterator it2 = v.begin();
+  while (it2 != v.end()) {
+    cout << *it2 << endl;
+    it2++;
   }
 
 }
